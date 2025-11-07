54c350ef312ab4fe5af90ae4d92864e772314729
jerbamichol-del <jerbamichol@gmail.com>
2025-11-07 01:39:26 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Fix globale: rimuove il focus "appiccicoso" prima del tap successivo
-import { installGlobalFirstTapFix } from './utils/mobileFocusFix';
-installGlobalFirstTapFix(document);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
