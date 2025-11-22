1cb916c9ee4427f1941840522d0e4663187cdd0f
jerbamichol-del <jerbamichol@gmail.com>
2025-11-22 21:58:40 +0100

    Update FingerprintIcon.tsx

--- components/icons/FingerprintIcon.tsx
+++ components/icons/FingerprintIcon.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export const FingerprintIcon: React.FC<React.SVGProps<SVGSVGElement>> = () => null;
+export const FingerprintIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0v.75a2.25 2.25 0 1 0 4.5 0v-.75a7.5 7.5 0 0 0-7.5-7.5 7.5 7.5 0 0 0-7.5 7.5c0 2.228-.344 4.378-.986 6.392" />
+  </svg>
+);
