4fe25b1852358e0f5db6a3493c49e2aa33ebd68c
jerbamichol-del <jerbamichol@gmail.com>
2025-12-02 21:38:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // AGGIUNGI QUESTA RIGA (sostituisci 'gestore' col nome esatto della tua repo se diverso)
+      base: '/gestore/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
