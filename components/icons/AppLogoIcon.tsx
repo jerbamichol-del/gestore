cfe9465822a86763ed33f2ba6c2ec3cde571bb8c
jerbamichol-del <jerbamichol@gmail.com>
2025-12-04 14:07:15 +0100

    Update AppLogoIcon.tsx

--- components/icons/AppLogoIcon.tsx
+++ components/icons/AppLogoIcon.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const AppLogoIcon: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = (props) => (
   <img
-    src="/logo.png"
+    // import.meta.env.BASE_URL contiene già "/gestore/" definito nel config
+    src={`${import.meta.env.BASE_URL}logo.png`} 
     alt="Gestore Spese Logo"
     {...props}
   />
