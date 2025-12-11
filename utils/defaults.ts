19e531ef92912b0babf81aa9b6f8bcccb544be04
jerbamichol-del <jerbamichol@gmail.com>
2025-12-11 14:54:53 +0100

    Update defaults.ts

--- utils/defaults.ts
+++ utils/defaults.ts
@@ -1,11 +1,12 @@
+
 import { Account } from '../types';
 
 export const DEFAULT_ACCOUNTS: Account[] = [
-  { id: 'cash', name: 'Contanti' },
-  { id: 'credit-card', name: 'Carta di Credito' },
-  { id: 'bank-account', name: 'Conto Bancario' },
-  { id: 'paypal', name: 'PayPal' },
-  { id: 'revolut', name: 'Revolut' },
-  { id: 'poste', name: 'PostePay' },
-  { id: 'crypto', name: 'Cripto' },
+  { id: 'cash', name: 'Contanti', icon: 'cash' },
+  { id: 'credit-card', name: 'Carta di Credito', icon: 'card' },
+  { id: 'bank-account', name: 'Conto Bancario', icon: 'bank' },
+  { id: 'paypal', name: 'PayPal', icon: 'paypal' },
+  { id: 'revolut', name: 'Revolut', icon: 'revolut' },
+  { id: 'poste', name: 'PostePay', icon: 'poste' },
+  { id: 'crypto', name: 'Cripto', icon: 'crypto' },
 ];
