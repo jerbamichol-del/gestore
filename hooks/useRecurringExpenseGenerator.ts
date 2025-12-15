e127e2e6a0c2092752bafaf4bbf848f63f0c9569
jerbamichol-del <jerbamichol@gmail.com>
2025-12-15 12:11:17 +0100

    Update useRecurringExpenseGenerator.ts

--- hooks/useRecurringExpenseGenerator.ts
+++ hooks/useRecurringExpenseGenerator.ts
@@ -1,5 +1,5 @@
 
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Expense } from '../types';
 import { parseLocalYYYYMMDD, toYYYYMMDD } from '../utils/date';
 
